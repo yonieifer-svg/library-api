b5c0336307461df56a224b0513ee62c79a1cc37a
Yonatan Eiferman <yonieifer@gmail.com>
2026-06-14 16:08:46 +0300

    book_db inherits from base_db

--- database/book_db.py
+++ database/book_db.py
@@ -1,116 +1,58 @@
 import mysql.connector
+from database.base_db import BaseRepo
+from database.db_connection import db
 
 
-class BookDB:
-    def __init__(self, conn: mysql.connector.MySQLConnection):
-        self.conn = conn
+
+class BookRepo(BaseRepo):
+    def __init__(self, db, table):
+        super().__init__(db, table)
+    
+    def create_book(self, data):
+        data["is_available"] = True
+        data["borrowed_by"] = None
+        return super().create(data)
+    
+    def get_all_books(self):
+        return super().find()
+    
+    def get_book_by_id(self, id: int):
+        return super().find({"id": id})[0]
+    
+    def update_book(self, id, data):
+        return super().update(id, data)
+    
+    def set_available(self, id:int , val:bool, member_id: int):
+        return super().update(id, {"is_available": val, "borrowed_by": member_id})
+    
+    def count_total_books(self):
+        return super().count()
+    
+    def count_available_books(self):
+        return super().count({"is_available": "True"})
+    
+    def count_borrowed_books(self):
+        return super().count({"is_available": "True"})
+    
+    def count_by_genre(self, genre):
+        return super().count({"genre": genre})
+    
+    def count_active_borrows_by_member(self, member_id):
+        return super().count({"borrowed_by": member_id})
+    
     
 
-    def create_book(self, data: dict):
-        try:
-            with self.conn.cursor() as cursor:
-
-                cursor.execute("""
-                    INSERT INTO books 
-                    (title, author, genre, is_available, borrowed_by_member_id)
-                    VALUES (%s, %s, %s, True, NULL)
-                """, [data["title"], data["author"], data["genre"]])
-
-                self.conn.commit()
-                return cursor.lastrowid
-        except mysql.connector.Error:
-            return None
 
 
-    def get_all_books(self):
-        with self.conn.cursor(dictionary=True) as cursor:
-            cursor.execute("SELECT * FROM books")
-            results = cursor.fetchall()
-            return results
 
 
-    def get_book_by_id(self, id: int):
-         with self.conn.cursor(dictionary=True) as cursor:
 
-            cursor.execute("""
-                SELECT * FROM books
-                WHERE id = %s
-            """, [id])
+book_repo = BookRepo(db.connection, "books")
 
-            result = cursor.fetchone()
-            return result
+# book_repo.find_by_id(5)
+
+# book_repo.create({"name": "harry potter"})
+
+# book_repo.update(1, {"name": "bible"})
 
 
-    def update_book(self, id: int, data: dict):
-        if not data:
-            return None
-        
-        with self.conn.cursor() as cursor:
-            keys = ", ".join(f"{k} = %s" for k in data.keys())
-            values = [v for v in data.values()] + [id]
-
-            cursor.execute(f"""
-                UPDATE books
-                SET {keys}
-                WHERE id = %s
-            """, values)
-            
-            self.conn.commit()
-            return cursor.rowcount > 0
-         
-
-    def set_available(self, id:int , val:bool, member_id: int):
-        with self.conn.cursor() as cursor:
-
-            cursor.execute("""
-                UPDATE books
-                SET is_available = %s, borrowed_by_member_id = %s
-                WHERE id = %s
-            """, [val, member_id, id])
-
-            self.conn.commit()
-            return cursor.rowcount > 0
-
-
-    def count_total_books(self):
-        with self.conn.cursor(dictionary=True) as cursor:
-            cursor.execute("SELECT COUNT(*)  AS total FROM books")
-            result = cursor.fetchone()
-            return result["total"]
-
-    def count_available_books(self):
-        with self.conn.cursor(dictionary=True) as cursor:
-            cursor.execute("""
-                SELECT COUNT(*) AS total FROM books 
-                WHERE is_available = True
-            """)
-            result = cursor.fetchone()
-            return result["total"]
-
-    def count_borrowed_books(self):
-        with self.conn.cursor(dictionary=True) as cursor:
-            cursor.execute("""
-                SELECT COUNT(*) AS total FROM books 
-                WHERE is_available = False
-            """)
-            result = cursor.fetchone()
-            return result["total"]
-
-    def count_by_genre(self, genre):
-        with self.conn.cursor(dictionary=True) as cursor:
-            cursor.execute("""
-                SELECT COUNT(*) AS total FROM books 
-                WHERE genre = %s
-            """, [genre])
-            result = cursor.fetchone()
-            return result["total"]
-
-
-    def count_active_borrows_by_member(self, member_id):
-        with self.conn.cursor(dictionary=True) as cursor:
-            cursor.execute("""
-                SELECT COUNT(*) AS total FROM books 
-                WHERE borrowed_by_member_id = %s
-            """, [member_id])
-            result = cursor.fetchone()
-            return result["total"]
