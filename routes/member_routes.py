d9e32f256b387ca05733544253f0712888aec948
Yonatan Eiferman <yonieifer@gmail.com>
2026-06-14 20:09:17 +0300

    add member routes

---
+++ routes/member_routes.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter
+from database.member_db import member_repo 
+
+router = APIRouter()
+
+@router.post("")
+def create_member(data: dict):
+    return member_repo.create_member(data)
+
+@router.get("")
+def get_all_members():
+    return member_repo.get_all_members()
+
+@router.get("/{id}")
+def get_member_by_id():
+    return member_repo.get_member_by_id(id)
+
+@router.patch("/{id}")
+def update_member(id: int, data: dict):
+    return member_repo.update_member(id, data)
+
+@router.patch("/{id}/deactivate")
+def deactive_member(id: int):
+    return member_repo.deactive_member(id)
+
+@router.patch("/{id}/activate")
+def active_member(id: int):
+    return member_repo.active_member(id)
